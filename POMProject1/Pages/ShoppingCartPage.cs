1bb9848fc52595f77638a5fa999be173aab76e1f
ObiOneKenobi <ObiOneKenobi@mail.pl>
2025-04-08 01:42:06 +0200

    NewTests

--- POMProject1/Pages/ShoppingCartPage.cs
+++ POMProject1/Pages/ShoppingCartPage.cs
@@ -5,28 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Playwright;
 
+
 namespace POMProject1.Pages
 {
-    public class ShoppingCart 
+    public class ShoppingCartPage
     {
 
         private IPage _page;
 
-        private readonly ILocator _backPackItem;
 
-        public ShoppingCart(IPage page) 
-        { 
+        public ShoppingCartPage(IPage page)
+        {
             _page = page;
 
             //playwright.Selectors.SetTestIdAttribute("data-test");
             //_backPackItem = _page.GetByTestId("inventory-item-name", new PageGetByTextOptions { Has }"Sauce Labs Backpack";
+        }
 
-            _backPackItem = _page.Locator(selector: "[data-test='inventory-item-name']", new PageLocatorOptions { HasTextString = "Sauce Labs Backpack" });
-
-
+        public async Task IfItemAddedToShoppingCart()
+        {
+            await backPackItem.ClickAsync();
+            await icnAddToCart.ClickAsync();
+            await icnShoppingCart.ClickAsync();
+            bool backPackItemVisibleInTheShoppingCart = await backPackItemInShoppingCart.IsVisibleAsync();
+            Assert.True(backPackItemVisibleInTheShoppingCart, "Backpack is added to the Shopping Cart");
 
         }
 
 
+        private ILocator backPackItem => _page.Locator(selector: "[data-test='inventory-item-name']", new PageLocatorOptions { HasTextString = "Sauce Labs Backpack" });
+        private ILocator icnShoppingCart => _page.Locator(selector: "a.shopping_cart_link[data-test='shopping-cart-link']");
+        private ILocator backPackItemInShoppingCart => _page.Locator(selector: "div.inventory_item_name[data-test='inventory-item-name']");
+        private ILocator icnAddToCart => _page.Locator(selector: "button[data-test='add-to-cart']");
     }
 }
--- POMProject1/Pages/SideBarPage.cs
+++ POMProject1/Pages/SideBarPage.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Playwright;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace POMProject1.Pages
 {
     public class SideBar
     {
-        private readonly ILocator _icnOptions;
+      
         private readonly ILocator _btnLogout;
         private readonly ILocator _btnAllItems;
         private readonly ILocator _btnAbout;
@@ -20,38 +21,37 @@ namespace POMProject1.Pages
         public SideBar(IPage page)
         {
             _page = page;
-
-            _icnOptions = _page.Locator(selector: "#react-burger-menu-btn");
-            _btnLogout = _page.Locator(selector: "#logout_sidebar_link");
-            _btnAllItems = _page.Locator(selector: "#inventory_sidebar_link");
-            _btnAbout = _page.Locator(selector: "#about_sidebar_link");
-            _btnResetAppState = _page.Locator(selector: "#reset_sidebar_link");
-            _targetUrl = "https://saucelabs.com/";
-
         }
 
         public async Task ChceckBtnAllItems() 
         { 
-            await _icnOptions.ClickAsync();
-            await _btnAllItems.ClickAsync();
-            await _btnAllItems.ClickAsync();
+            await ICNOptions.ClickAsync();
+            await btnAllItems.ClickAsync();
+            await btnAllItems.ClickAsync();
         }
         public async Task ChceckBtnResetAppState()
         {
-            await _icnOptions.ClickAsync();
-            await _btnResetAppState.IsEnabledAsync();
-            await _btnResetAppState.ClickAsync();
+            await ICNOptions.ClickAsync();
+            await btnResetAppState.IsEnabledAsync();
+            await btnResetAppState.ClickAsync();
         }
         public async Task ChceckBtnAbout()
         {
-            await _icnOptions.ClickAsync();
-            await _btnAbout.IsEnabledAsync();
-            await _btnAbout.ClickAsync();
+            await ICNOptions.ClickAsync();
+            await btnAbout.IsEnabledAsync();
+            await btnAbout.ClickAsync();
         }
 
         public async Task IsCurrentUrlDifferentBtnAbout()
         {
             await _page.WaitForURLAsync(_targetUrl);
         }
+
+        private ILocator ICNOptions => _page.Locator(selector: "#react-burger-menu-btn");
+        private ILocator btnLogout => _page.Locator(selector: "#logout_sidebar_link");
+        private ILocator btnAllItems => _page.Locator(selector: "#inventory_sidebar_link");
+        private ILocator btnAbout => _page.Locator(selector: "#about_sidebar_link");
+        private ILocator btnResetAppState => _page.Locator(selector: "#reset_sidebar_link");
+
     }
 }
